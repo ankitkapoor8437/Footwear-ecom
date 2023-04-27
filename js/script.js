4a159cccfb522560d7d53f968a11720c92a8d136
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-04-27 12:34:11 +0530

    Completed the Header section and and hero section

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// add sticky navigation bar
+
+// activate the hamburger menu and mobile navigation
+const hamb = document.querySelector(".hamb");
+const nav = document.querySelector(".mobile-nav");
+
+hamb.addEventListener("click", function () {
+    this.classList.toggle("active");
+    nav.classList.toggle("slide");
+
+});
+
+
+
+
+// website scrollReveal effect
